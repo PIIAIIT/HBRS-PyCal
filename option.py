9a8836145e665229cd69501c7e718c083d1f77c9
Patrick Saft <73080771+PIIAIIT@users.noreply.github.com>
2024-04-27 00:49:47 +0200

    Update option.py
    Changed Options and OptionList

--- option.py
+++ option.py
@@ -1,24 +1,23 @@
 
 class Option:
 
-    def __init__(self, studiengang: str, semester: int, veranstaltung: str="", professor: str ="") -> None:
+    def __init__(self, studiengang: str, semester: int) -> None:
         r"""Creates a Option object.
         :param studiengang: Studiengang den du besuchst (BCSP, BI, BWI, ZUSATZ(für Zusatzveranstaltungen)
         :param semester: Semester in dem du dich befindest (1, 2, 3, 4, 5, 6)
-        :param veranstaltung: (Optional) Name der Veranstaltung
-        :param professor: (Optional) Name des Professors
-        - Example: Option("BI", 2, "Programmierung in C", "Böhmer")
+        - Example: Option("BI", 2)
         """
         self.studiengang = studiengang
         self.semester = semester
-        self.veranstaltung = veranstaltung
-        self.professor = professor
     
 class OptionList:
 
     options: Option = []
 
     def __init__(self, *options: Option) -> None:
+        """Creates an OptionList object.
+        :param options: The options to add to the list
+        - Example: OptionList(Option("BWI", 4), Option("BI", 2))"""
         for arg in options:
             self.options.append(arg)
     
